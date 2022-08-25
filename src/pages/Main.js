e852f082d933c48ca21e14851d62ae979894168a
dlawjdghks11 <85840643+dlawjdghks11@users.noreply.github.com>
2022-08-26 01:21:12 +0900

    Feat: Main.js CSS

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { getDocs, collection } from 'firebase/firestore';
 import { dbService } from '../util/fbase';
 import { useEffect } from 'react';
+import Item from '../components/Item';
 
 function Main() {
   const [dbData, setDbData] = useState([]);
@@ -21,14 +22,28 @@ function Main() {
     };
     getData();
   }, []);
-
+  console.log(dbData)
   return (
     <div>
       <Header />
+      <BackgroundContainer>
+        {dbData.length === 0 ? null 
+        : dbData.map((item, idx) => <Item key={idx} data={item}/>)}
+      </BackgroundContainer>
 
       <ChannelTalk />
     </div>
   );
 }
+
+const BackgroundContainer = styled.div`
+    width: 900px;
+    height: auto;
+    margin-top: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    flex-wrap: wrap;
+`;
 
 export default Main;
