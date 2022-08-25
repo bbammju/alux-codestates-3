a3a130d572aab6c2f0f92edcc8941b2aa0f78d8c
bbammju <stwinsilver2@gmail.com>
2022-08-25 15:11:12 +0900

    1st detailpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,15 @@ import React from "react";
 import Logo from "../assets/logo.png"
 import Faq from "../assets/faq.png"
 
-
 const HeaderContainer = styled.div`
   width: 99vw;
   height: 15vh;
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
-  padding: 5px;
+  justify-content: space-between;
+  padding-left: 5px;
+  padding-top: 5px;
+
 `
 const LogoContainer = styled.img`
   display: flex;
@@ -24,15 +25,19 @@ const LogoContainer = styled.img`
 const BrandInfoContainer = styled.div`
   display: flex;
   max-height: 9vh;
-  max-width: 17vw;
+  max-width: 20vw;
   align-items: center;
   font-size: 2rem;
 `
-const FAQButton = styled.img`
+const FAQContainer = styled.img`
   display: flex;
   height: 15vh;
   width: 6.5vw;
   border-radius: 1rem;
+  margin-right: 4vw;
+`
+const LBContainer = styled.div`
+  display: flex;
 `
 
 const Header = () => {
@@ -40,9 +45,11 @@ const Header = () => {
   return(
     
     <HeaderContainer>
-      <LogoContainer src={Logo}/>
-      <BrandInfoContainer>Water 다운로드 센터</BrandInfoContainer>
-      <FAQButton src={Faq}/>
+      <LBContainer>
+        <LogoContainer src={Logo}/>
+        <BrandInfoContainer>Water 다운로드 센터</BrandInfoContainer>
+      </LBContainer>
+      <FAQContainer src={Faq}/>
     </HeaderContainer>
     
   )
